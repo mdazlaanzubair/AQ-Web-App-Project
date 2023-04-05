5895252e467c201aed7ca89fbc8891b5997389be
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-04-05 22:43:27 +0500

    FAQ collapse also fixed

--- src/components/landing-page/home/FAQ.jsx
+++ src/components/landing-page/home/FAQ.jsx
@@ -4,35 +4,32 @@ const FAQ = () => {
   const { appContent } = useContentContext();
   return (
     <section id="hero-section" className="py-3">
-      <div className="hero bg-base-200 rounded-box">
-        <div className="hero-content text-center pt-10">
-          <div className="max-w-3xl md:max-w-lg lg:max-w-3xl">
+      <div className="hero bg-base-100 rounded-box">
+        <div className="hero-content flex-col lg:flex-row pt-10 justify-center items-start">
+          <div className="max-w-full">
             <h1 className="text-5xl font-bold">
               {appContent?.faq_section?.title}
             </h1>
             <p className="py-6">{appContent?.faq_section?.para}</p>
           </div>
-        </div>
-      </div>
-
-      <div className="hero bg-base-200 rounded-box">
-        <div className="hero-content text-center pb-10">
-          <div className="max-w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-start">
-
-            {appContent?.faq_section?.faqs?.map((faq, index) => (
-              <div
-                key={index}
-                tabIndex={0}
-                className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  {faq?.q}
+          <div className="max-w-full">
+            <div className="grid grid-cols-1">
+              {appContent?.faq_section?.faqs?.map((faq, index) => (
+                <div
+                  key={index}
+                  tabIndex={0}
+                  className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mb-3"
+                >
+                  <input id={index} type="checkbox" className="peer" />
+                  <div className="collapse-title text-xl font-medium peer-checked:bg-primary peer-checked:text-primary-content">
+                    {faq?.q}
+                  </div>
+                  <div className="collapse-content peer-checked:bg-primary peer-checked:text-primary-content">
+                    <p>{faq?.a}</p>
+                  </div>
                 </div>
-                <div className="collapse-content">
-                  <p>{faq?.a}</p>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
