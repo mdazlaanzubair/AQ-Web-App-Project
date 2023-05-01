8c8d216e0b67e53a40442afbc42b27244bbe6449
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-05-01 20:09:51 +0500

    UI updated

--- src/components/dashboard/content/HeaderContent.jsx
+++ src/components/dashboard/content/HeaderContent.jsx
@@ -22,8 +22,9 @@ const HeaderContent = ({}) => {
   return (
     <section id="header-content" className="py-3">
       <h1 className="text-start text-3xl mb-5 font-bold">Header Content</h1>
-      <div className="w-full grid grid-cols-1 gap-3">
-        <div className="flex flex-col w-full">
+      <div className="w-full grid grid-cols-1 gap-3"></div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 mb-5 gap-3">
+        <div className="col flex flex-col">
           <div className="col mb-5 w-full">
             <h2 className="text-xl font-medium mb-3">Headline</h2>
             <input
@@ -64,27 +65,27 @@ const HeaderContent = ({}) => {
             </button>
           </div>
         </div>
-      </div>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 mb-5 gap-3">
-        {headerContent?.map((item, index) => (
-          <div
-            className="card w-full bg-base-100 shadow-xl image-full cursor-pointer"
-            key={index}
-            onClick={() => setSelectedContent(item)}
-          >
-            <figure>
-              <img src={item.img} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="mb-5 text-xl md:text-3xl font-bold lg:text-5xl">
-                {item.title}
-              </h2>
-              <p className="mb-5 text-xs md:text-xl lg:text-2xl font-light md:font-normal">
-                {item.para}
-              </p>
+        <div className="col col-span-2 grid grid-cols-1 md:grid-cols-2 gap-3">
+          {headerContent?.map((item, index) => (
+            <div
+              className="card shadow-xl image-full cursor-pointer bg-opacity-0"
+              key={index}
+              onClick={() => setSelectedContent(item)}
+            >
+              <figure>
+                <img src={item.img} alt="Shoes" />
+              </figure>
+              <div className="card-body bg-opacity-0">
+                <h2 className="mb-5 text-xl md:text-3xl font-bold">
+                  {item.title}
+                </h2>
+                <p className="mb-5 text-xs md:text-xl font-light md:font-normal">
+                  {item.para}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
